94d7acc945301a36265093884e02e12c5a3c5f0f
mohammad <nasiri.mohammadreza92@yahoo.com>
2023-12-30 10:17:32 -0800

    dsd

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import ProgresChart from './component/progressChart/progressChart';
+import Menu from './component/menu/menu';
+
+
+export default function App() {
+  return (
+    <>
+      {/* <ProgresChart /> */}
+      <Menu />
+      
+      
+    </>
+  );
+}
